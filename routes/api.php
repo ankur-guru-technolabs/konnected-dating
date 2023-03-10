aa2d33b179861de137008a8462949d3c6b2ddd85
ankur-guru-technolabs <ankur.gurutechnolabs@gmail.com>
2023-03-10 19:17:24 +0530

    create static table and project setup

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::post('register', [AuthController::class, 'register'])->name('register');
+Route::post('login', [AuthController::class, 'login'])->name('login');
+ 
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
