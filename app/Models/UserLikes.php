fbc25391b886d7a32582347f0983a4607a0354d9
ankur-guru-technolabs <ankur.gurutechnolabs@gmail.com>
2023-05-08 19:11:34 +0530

    user likes modal upload

---
+++ app/Models/UserLikes.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserLikes extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'like_from',
+        'like_to',
+        'match_id',
+        'match_status',
+        'status',
+        'matched_at',
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class,'id','like_from');
+    }
+}
